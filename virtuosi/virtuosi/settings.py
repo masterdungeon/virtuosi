275b8f752bf0ccdd95065c4981ff05421dd859e8
masterdungeon <rohit.pandey@prolitus.com>
2020-12-12 23:31:21 +0530

    django setting updated for heroku

--- virtuosi/virtuosi/settings.py
+++ virtuosi/virtuosi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -128,3 +129,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Heroku Settings
+django_heroku.settings(locals())
